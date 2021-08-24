be6f31492f699f602241c512ca2e9394417c2c0e
lohba <lukeohba@gmail.com>
2021-08-23 22:32:48 -0400

    error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,80 @@
+use std::str::Utf8Error;
+use std::str;
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method,
+
+pub struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    // Get /search?name-abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
+
+
+
+// trait Encrypt {
+//     fn encrypt(&self) -> Self;
+// }
+
+// impl Encrypt for String {
+//     fn encrypt(&self) -> Self {
+//         unimplemented!()
+//     }
+// }
+
+// impl Encrypt for &[u8] {
+//     fn encrypt(&self) -> Self {
+//         unimplemented!()
+//     }
+// }
